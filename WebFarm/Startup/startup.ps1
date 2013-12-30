814351a6a6ef8b70cb16f5ab1febe63756339b51
Rob Moore <me@robdmoore.id.au>
2013-12-30 09:36:18 +0800

    Dirty hack that didn't work - manually performing Tentacle.exe commands

--- WebFarm/Startup/startup.ps1
+++ WebFarm/Startup/startup.ps1
@@ -1,14 +1,5 @@
 ﻿$ErrorActionPreference = "Stop"
 
-function Run($command, $arguments) {
-    Write-Output "Running $command with $arguments"
-    & $command $arguments
-    if ($LastExitCode -ne 0) {
-        throw "Failed to $command with $arguments"
-    }
-    Write-Output "Successfully ran $command with $arguments"
-}
-
 try
 {
     $PathToInstall = $env:PathToInstall
@@ -34,6 +25,7 @@ try
         (new-object System.Net.WebClient).DownloadFile($TentacleDownloadPath, $TentacleInstallerPath)
         Write-Output "Downloaded $TentacleDownloadPath to $TentacleInstallerPath"
     }
+    #cp c:\users\robert\desktop\Octopus.Tentacle.msi $TentacleInstallerPath
     
     $app = Get-WmiObject -Class Win32_Product -Filter "Name = 'Octopus Deploy Tentacle'"
     if ($app -ne $null) {
@@ -41,34 +33,61 @@ try
         $app.Uninstall()
     }
 
+    Write-Output "Creating service 'OctopusDeploy Tentacle'"
+    if ((Get-Service -Name "OctopusDeploy Tentacle" -ErrorAction SilentlyContinue) -ne $null) {
+        sc.exe delete "OctopusDeploy Tentacle"
+    }
+    New-Service -Name "OctopusDeploy Tentacle" -DisplayName "OctopusDeploy Tentacle" -Description "Octopus Deploy: Tentacle deployment agent" -StartupType Automatic -BinaryPathName ('"' + $TentaclePath + '" run --instance="' + $instance + '"')
+
     Write-Output "Installing $TentacleInstallerPath to $tentacleDir"
     Start-Process "msiexec" -ArgumentList "INSTALLLOCATION=""$TentacleDir"" /i ""$TentacleInstallerPath"" /quiet" -Wait
 
-    Write-Output "Executing $TentaclePath create-instance --instance $instance --config $ConfigPath"
-    & $TentaclePath create-instance --instance $instance --config $ConfigPath
-    if ($LastExitCode -ne 0) {
-        throw "Command failed"
-    }
-    Write-Output "Executing $TentaclePath configure --instance $instance --home $PathRoDeployments --console"
-    & $TentaclePath configure --instance $instance --home $PathToDeployments --console
-    if ($LastExitCode -ne 0) {
-        throw "Command failed"
-    }
-    Write-Output "Executing $TentaclePath configure --instance $instance --app ""$AppsPath --console"
-    & $TentaclePath configure --instance $instance --app ""$AppsPath --console
-    if ($LastExitCode -ne 0) {
-        throw "Command failed"
-    }
-    Write-Output "Executing $TentaclePath register-with --instance $instance --server $OctopusServer --environment $TentacleEnvironment --role $TentacleRole --apiKey $OctopusApiKey --comms-style TentacleActive --force --console"
-    & $TentaclePath register-with --instance $instance --server $OctopusServer --environment $TentacleEnvironment --role $TentacleRole --apiKey $OctopusApiKey --comms-style TentacleActive --force --console
-    if ($LastExitCode -ne 0) {
-        throw "Command failed"
-    }
-    Write-Output "Executing $TentaclePath service --instance $instance --install --start --console"
-    & $TentaclePath service --instance $instance --install --start --console
-    if ($LastExitCode -ne 0) {
-        throw "Command failed"
-    }
+    #Write-Output "Executing $TentaclePath create-instance --instance $instance --config $ConfigPath"
+    #& $TentaclePath create-instance --instance $instance --config $ConfigPath
+    #if ($LastExitCode -ne 0) {
+    #    throw "Command failed"
+    #}
+    "<?xml version='1.0' encoding='UTF-8' ?><octopus-settings></octopus-settings>" | Out-File $ConfigPath -Force
+    New-Item -Path HKLM:\Software\Octopus\Tentacle -Name $instance –Force
+    Set-ItemProperty -Path HKLM:\Software\Octopus\Tentacle\$instance -Name "ConfigurationFilePath" -Value "$ConfigPath"
+
+
+    #Write-Output "Executing $TentaclePath configure --instance $instance --home $PathRoDeployments --console"
+    #& $TentaclePath configure --instance $instance --home $PathToDeployments --console
+    #if ($LastExitCode -ne 0) {
+    #    throw "Command failed"
+    #}
+    [xml]$xml = Get-Content $ConfigPath
+    $setting = $xml.CreateElement("set")
+    $setting.SetAttribute("key", "Octopus.Home")
+    $setting.InnerText = "$PathToDeployments"
+    $xml.SelectSingleNode("./octopus-settings").AppendChild($setting)
+    $xml.Save("$ConfigPath")
+
+
+    #Write-Output "Executing $TentaclePath configure --instance $instance --app ""$AppsPath --console"
+    #& $TentaclePath configure --instance $instance --app ""$AppsPath --console
+    #if ($LastExitCode -ne 0) {
+    #    throw "Command failed"
+    #}
+    [xml]$xml = Get-Content $ConfigPath
+    $setting = $xml.CreateElement("set")
+    $setting.SetAttribute("key", "Tentacle.Deployment.ApplicationDirectory")
+    $setting.InnerText = "$AppsPath"
+    $xml.SelectSingleNode("./octopus-settings").AppendChild($setting)
+    $xml.Save("$ConfigPath")
+
+
+    #Write-Output "Executing $TentaclePath register-with --instance $instance --server $OctopusServer --environment $TentacleEnvironment --role $TentacleRole --apiKey $OctopusApiKey --comms-style TentacleActive --force --console"
+    #& $TentaclePath register-with --instance $instance --server $OctopusServer --environment $TentacleEnvironment --role $TentacleRole --apiKey $OctopusApiKey --comms-style TentacleActive --force --console
+    #if ($LastExitCode -ne 0) {
+    #    throw "Command failed"
+    #}
+    #Write-Output "Executing $TentaclePath service --instance $instance --install --start --console"
+    #& $TentaclePath service --instance $instance --install --start --console
+    #if ($LastExitCode -ne 0) {
+    #    throw "Command failed"
+    #}
 
     exit 0
 }
