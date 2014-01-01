2a30410d32cabe24ffb295bddf0c96a16e72b942
Rob Moore <me@robdmoore.id.au>
2014-01-01 12:48:13 +0800

    Gave IIS App Pool users access to the deployments directory

--- WebFarm/Startup/startup.ps1
+++ WebFarm/Startup/startup.ps1
@@ -5,13 +5,15 @@ try
     $TentacleDownloadPath = "http://download.octopusdeploy.com/octopus/Octopus.Tentacle.2.0.6.950.msi"
     $TentacleLocalDebuggingPath = "C:\Octopus.Tentacle.msi"
     $PathToInstall = $env:PathToInstall
+    $PathToDeployments = $env:PathToDeployments
     $TentacleInstallerPath = Join-Path $PathToInstall "Octopus.Tentacle.msi"
-    $emulated = $env:ComputeEmulatorRunning
+    $ApplicationsPath = Join-Path $PathToDeployments "Applications"
+    $Emulated = $env:ComputeEmulatorRunning
     $TentacleDir = Join-Path $PathToInstall "Tentacle"
 
     if (Test-Path $TentacleInstallerPath) {
         Write-Output "Already found tentacle installer at $TentacleInstallerPath"
-    } elseif (($emulated -eq "true") -and (Test-Path $TentacleLocalDebuggingPath)) {
+    } elseif (($Emulated -eq "true") -and (Test-Path $TentacleLocalDebuggingPath)) {
         Write-Output "Copying $TentacleLocalDebuggingPath to $TentacleInstallerPath"
         cp $TentacleLocalDebuggingPath $TentacleInstallerPath
     } else {
@@ -20,14 +22,23 @@ try
         Write-Output "Downloaded $TentacleDownloadPath to $TentacleInstallerPath"
     }
     
-    $app = Get-WmiObject -Class Win32_Product -Filter "Name = 'Octopus Deploy Tentacle'"
-    if ($app -ne $null) {
+    $App = Get-WmiObject -Class Win32_Product -Filter "Name = 'Octopus Deploy Tentacle'"
+    if ($App -ne $null) {
         Write-Output "OctopusDeploy Tentacle already installed; uninstalling..."
-        $app.Uninstall()
+        $App.Uninstall()
     }
 
     Write-Output "Installing $TentacleInstallerPath to $TentacleDir"
     Start-Process "msiexec" -ArgumentList "INSTALLLOCATION=""$TentacleDir"" /i ""$TentacleInstallerPath"" /quiet" -Wait
+
+    if (-not (Test-Path $ApplicationsPath)) {
+        New-Item -type directory -path "$ApplicationsPath"
+    }
+    $Acl = Get-Acl "$ApplicationsPath"
+    $IisAppPoolAccess = New-Object System.Security.AccessControl.FileSystemAccessRule("IIS_IUSRS", "FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")
+    $Acl.SetAccessRule($IisAppPoolAccess)
+    Set-Acl "$ApplicationsPath" -AclObject $Acl
+    Get-ChildItem "$ApplicationsPath" -Recurse -Force | Set-Acl -AclObject $Acl
 
     exit 0
 }
