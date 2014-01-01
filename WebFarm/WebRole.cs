34836c90a9be6db215c38729d5f812f3a2c59527
Rob Moore <me@robdmoore.id.au>
2014-01-01 14:04:58 +0800

    Enriching log messages with the role instance and making the role request that octopus deploy to it before returning from OnStart

--- WebFarm/WebRole.cs
+++ WebFarm/WebRole.cs
@@ -6,7 +6,11 @@ using System.Threading;
 using Microsoft.WindowsAzure.ServiceRuntime;
 using Microsoft.WindowsAzure.Storage;
 using Octopus.Client;
+using Octopus.Client.Model;
+using Octopus.Platform.Model;
 using Serilog;
+using Serilog.Core;
+using Serilog.Events;
 
 namespace WebFarm
 {
@@ -20,14 +24,14 @@ namespace WebFarm
         private readonly string _tentacleRole;
         private readonly string _name;
 
-        private static string[] _roleConfigurations = new[] {"OctopusServer", "OctopusApiKey", "TentacleEnvironment", "TentacleRole"};
+        private static readonly string[] RoleConfigurations = new[] {"OctopusServer", "OctopusApiKey", "TentacleEnvironment", "TentacleRole"};
 
         public WebRole()
         {
             _log = new LoggerConfiguration()
                 .WriteTo.AzureTableStorage(CloudStorageAccount.Parse(RoleEnvironment.GetConfigurationSettingValue("Microsoft.WindowsAzure.Plugins.Diagnostics.ConnectionString")))
+                .Enrich.With<RoleEnvironmentEnricher>()
                 .CreateLogger();
-            // todo: enrich with the instance name
 
             _octopusServer = RoleEnvironment.GetConfigurationSettingValue("OctopusServer");
             _octopusApiKey = RoleEnvironment.GetConfigurationSettingValue("OctopusApiKey");
@@ -39,7 +43,6 @@ namespace WebFarm
                 : string.Format("{0}_{1}", RoleEnvironment.CurrentRoleInstance.Id, _tentacleEnvironment);
 
             RoleEnvironment.Changing += RoleEnvironmentChanging;
-            // todo: request recycle for the above configuration settings being changed
 
             var endpoint = new OctopusServerEndpoint(_octopusServer, _octopusApiKey);
             _repository = new OctopusRepository(endpoint);
@@ -59,7 +62,7 @@ namespace WebFarm
             Run(tentaclePath, string.Format("register-with {0} --server \"{1}\" --environment \"{2}\" --role \"{3}\" --apiKey \"{4}\" --name \"{5}\" --comms-style TentacleActive --force --console", instanceArg, _octopusServer, _tentacleEnvironment, _tentacleRole, _octopusApiKey, _name));
             Run(tentaclePath, string.Format("service {0} --install --start --console", instanceArg));
 
-            // todo: Redeploy sites to it
+            DeployAllCurrentReleasesToThisRole();
 
             return true;
         }
@@ -78,7 +81,7 @@ namespace WebFarm
             //  then we need to recycle the role so it re-registers with the Octopus Server
             var recycle = e.Changes
                 .OfType<RoleEnvironmentConfigurationSettingChange>()
-                .Any(c => _roleConfigurations.Contains(c.ConfigurationSettingName));
+                .Any(c => RoleConfigurations.Contains(c.ConfigurationSettingName));
             if (recycle)
                 RoleEnvironment.RequestRecycle();
         }
@@ -128,6 +131,50 @@ namespace WebFarm
                 _log.Error(e, "Failed to execute {executable} with {arguments}", executable, arguments);
                 throw;
             }
+        }
+
+        private void DeployAllCurrentReleasesToThisRole()
+        {
+            var environment = _repository.Environments.FindByName(_tentacleEnvironment).Id;
+
+            var dashboard = _repository.Dashboards.GetDashboard();
+            var releaseTasks = dashboard.Items
+                .Where(i => i.EnvironmentId == environment)
+                .ToList()
+                .Select(currentRelease => _repository.Deployments.Create(
+                    new DeploymentResource
+                    {
+                        Comments = "Automated deployment by " + RoleEnvironment.CurrentRoleInstance.Id,
+                        EnvironmentId = currentRelease.EnvironmentId,
+                        ReleaseId = currentRelease.ReleaseId
+                    }
+                ))
+                .Select(d => d.TaskId)
+                .ToList();
+            _log.Information("Triggered deployments with task ids: {taskIds}", releaseTasks);
+
+            var taskStatuses = releaseTasks.Select(taskId => _repository.Tasks.Get(taskId).State);
+
+            var currentStatuses = taskStatuses.ToList();
+            while (currentStatuses.Any(s => s == TaskState.Executing || s == TaskState.Queued))
+            {
+                _log.Debug("Waiting for deployments; current statuses: {statuses}", currentStatuses);
+                Thread.Sleep(1000);
+                currentStatuses = taskStatuses.ToList();
+            }
+
+            _log.Information("Deployments completed with statuses: {statuses}", currentStatuses);
+
+            if (currentStatuses.Any(s => s == TaskState.Failed || s == TaskState.TimedOut))
+                throw new Exception("Failed to deploy to this role - at least one necessary deployment either failed or timed out - recycling role to try again");
+        }
+    }
+
+    class RoleEnvironmentEnricher : ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            logEvent.AddOrUpdateProperty(propertyFactory.CreateProperty("InstanceId", RoleEnvironment.CurrentRoleInstance.Id));
         }
     }
 }
